5b8401af61b5f1e837a08855d0daaf1ce2417270
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-01-15 17:02:28 +0000

    Remove main.js dependency

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -53,15 +53,17 @@ $(document).ready(function(){
   */
 
   // Follow menu drop down
-
+  /*
   $(".author__urls-wrapper button").on("click", function() {
     $(".author__urls").fadeToggle("fast", function() {});
     $(".author__urls-wrapper button").toggleClass("open");
   });
+  */
 
   // init smooth scroll
-  $("a").smoothScroll({offset: -20});
+  // $("a").smoothScroll({offset: -20});
 
+  /*
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
@@ -95,6 +97,6 @@ $(document).ready(function(){
     },
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-  });
+  });*/
 
 });
